195ed3707a79b4d4105200411a3c98e862b48ec7
RizkiPratama017 <rizki.223040017@mail.unpas.ac.id>
2024-10-09 15:50:23 +0700

    OOP - video Abstrack

---
+++ OOP/Abstrack/abstrack-class.php
@@ -0,0 +1,162 @@
+<?php
+
+abstract class Produk
+{
+    private $judul,
+        $penulis,
+        $penerbit,
+        $harga,
+        $diskon = 0;
+
+
+
+    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getJudul()
+    {
+        return $this->judul;
+    }
+
+    public function setJudul($judul)
+    {
+        // if (!is_string($judul)) {
+        //     throw new Exception("Judul harus STRING");
+        // }
+        $this->judul = $judul;
+    }
+
+    public function getPenulis()
+    {
+        return $this->penulis;
+    }
+
+    public function setPenulis($penulis)
+    {
+        $this->penulis = $penulis;
+    }
+
+    public function getpenerbit()
+    {
+        return $this->penerbit;
+    }
+
+    public function setPenerbit($penerbit)
+    {
+        $this->penerbit = $penerbit;
+    }
+
+    public function getharga()
+    {
+        return $this->harga - ($this->harga * $this->diskon / 100);
+    }
+
+    public function setHarga($harga)
+    {
+        $this->penerbit = $harga;
+    }
+
+    public function diskon()
+    {
+        return $this->diskon;
+    }
+
+    public function setdiskon($diskon)
+    {
+        $this->diskon = $diskon;
+    }
+
+
+
+    public function getlabel()    //method
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+
+
+    abstract public function getInfoProduk();
+    public function getInfo()
+    {
+        $str = "{$this->judul} | {$this->getlabel()} (Rp. {$this->harga})";
+
+        return $str;
+    }
+}
+
+class Komik extends Produk
+{
+    public $jmlHalaman;
+
+    public function __construct($judul, $penulis, $penerbit, $harga, $jmlHalaman)
+    {
+        parent::__construct($judul, $penulis, $penerbit, $harga, $jmlHalaman);
+
+        $this->jmlHalaman = $jmlHalaman;
+    }
+
+
+    public function getInfoProduk()
+    {
+        $str = "Komik : " . $this->getInfo() . " - {$this->jmlHalaman} Halaman";
+        return $str;
+    }
+}
+
+class Game extends Produk
+{
+    public $waktuMain;
+
+    public function __construct($judul, $penulis, $penerbit, $harga, $waktuMain)
+    {
+        parent::__construct($judul, $penulis, $penerbit, $harga, $waktuMain);
+
+        $this->waktuMain = $waktuMain;
+    }
+
+
+
+    public function getInfoProduk()
+    {
+        $str = "Game : " . $this->getInfo() . " - {$this->waktuMain} Jam";
+        return $str;
+    }
+}
+
+
+class CetakInfoProduk
+{
+    public $daftarProduk = [];
+
+    public function tambahProduk(Produk $produk)
+    {
+        $this->daftarProduk[] = $produk;
+    }
+
+
+    public function cetak()
+    {
+        $str = "Daftar Produk : <br>";
+
+        foreach ($this->daftarProduk as $p) {
+            $str .= "-{$p->getInfoProduk()} <br>";
+        }
+
+        return $str;
+    }
+}
+
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 50);
+
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+
+echo $cetakProduk->cetak();
