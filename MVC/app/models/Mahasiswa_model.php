d4bfce2e895d87f483cc31dbc3b734c5bc3c6947
RizkiPratama017 <rizki.223040017@mail.unpas.ac.id>
2024-10-19 12:23:34 +0700

    MVC - Video insert data

--- MVC/app/models/Mahasiswa_model.php
+++ MVC/app/models/Mahasiswa_model.php
@@ -11,14 +11,27 @@ class Mahasiswa_model
 
     public function getAllMahasiswa()
     {
-        $this->db->query('SELECT * FROM ' . $this->table); // Added space after FROM
+        $this->db->query('SELECT * FROM ' . $this->table);
         return $this->db->resultSet();
     }
 
     public function getMahasiswaById($id)
     {
-        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id'); // Added space before WHERE
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
         $this->db->bind('id', $id);
         return $this->db->single();
+    }
+
+    public function tambahDataMahasiswa($data)
+    {
+        $query = "INSERT INTO mahasiswa (nama, nrp, email, jurusan) VALUES (:nama, :nrp, :email, :jurusan)";
+        $this->db->query($query);
+
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('nrp', $data['nrp']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('jurusan', $data['jurusan']);
+        $this->db->execute();
+        return $this->db->rowCount();
     }
 }
