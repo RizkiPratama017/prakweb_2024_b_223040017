a1792abcef5d223d66ffc5a3c2d79f6087343cdc
RizkiPratama017 <rizki.223040017@mail.unpas.ac.id>
2024-10-19 08:50:28 +0700

    MVC - Video model

--- MVC/app/views/home/index.php
+++ MVC/app/views/home/index.php
@@ -1,5 +1,6 @@
 <div class="container">
-    <div class="jumbotron">
+    <div class="jumbotron mt-4">
         <h1 class="display-4">Selamat datang di webiste saya</h1>
+        <p class="lead">Halo, nama saya <?= $data['nama']; ?></p>
     </div>
 </div>
