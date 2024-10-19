0e61ee9adb462a2ca60a4d47965c5670089dbdbc
RizkiPratama017 <rizki.223040017@mail.unpas.ac.id>
2024-10-19 10:32:29 +0700

    MVC - video Database Wrapper

--- MVC/app/views/mahasiswa/index.php
+++ MVC/app/views/mahasiswa/index.php
@@ -4,13 +4,12 @@
             <h3>Daftar Mahasiswa</h3>
 
             <?php foreach ($data['mhs'] as $mhs) : ?>
-                <ul>
-                    <li><?= $mhs['nama']; ?></li>
-                    <li><?= $mhs['nrp']; ?></li>
-                    <li><?= $mhs['email']; ?></li>
-                    <li><?= $mhs['jurusan']; ?></li>
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <?= $mhs['nama']; ?>
+                        <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge bg-primary">detail </a>
+                    </li>
                 </ul>
-
             <?php endforeach ?>
         </div>
     </div>
